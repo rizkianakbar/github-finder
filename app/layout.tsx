b651af4ac25efd0968328eaf3dea2f82e6b76578
rizkianakbar <rizkianakbar4@gmail.com>
2023-06-14 03:33:24 +0700

    chore: add react-hot-toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Metrophobic } from "next/font/google";
+import { Toaster } from "react-hot-toast";
 
 import "../styles/globals.css";
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main className="max-w-xl min-h-screen p-2 mx-auto bg-white lg:p-4">
+          <Toaster />
           {children}
         </main>
       </body>
